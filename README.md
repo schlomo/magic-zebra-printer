7348bb4be92a80ef5eb2d3a2aba03cb6decc0d8b
Schlomo Schapiro <schlomo@schapiro.org>
2021-07-13 15:57:53 +0200

    initial

---
+++ README.md
@@ -0,0 +1,47 @@
+# Magic Zebra Printer
+
+Simple script to automatically determine the paper length for a PDF file (based on the 1st page) and print it on a label printer.
+
+My Zebra label printer has a roll of continuous paper and a cutter so that it can print any page size by simply cutting off the paper at the appropriate length.
+
+My problem is that I use it to print all sorts of shipping labels or other small info pages that have different page sizes. Previously I used to create many different custom page sizes to accomodate different print jobs. With this script everything happens automatically:
+
+* Scale down the page to 4 inch wide
+* Determine the page length required for the print job
+* Determine the printer (looking for `Zebra*`) to use
+* Should work on Mac, Linux & Windows
+* Includes PDF Service and Service and Application for MacOS
+
+## Installation
+
+1. Make sure to have Python 3.6 or later installed.
+
+2. Checkout this Git repo into a directory of your choice, I use `/Users/schlomoschapiro/src/magic-zebra-printer` and you will have to adjust that later.
+
+3. Create a Python 3 Virtual Environment named `venv` via `python3 -m venv venv`.
+
+4. Activate it with `source venv/bin/activate` and install the Python module dependencies with `pip install -r requirements.txt`.
+
+For MacOS to install the integrations:
+
+1. Copy the content of [Applications](Applications) to your `$HOME` directory. 
+
+2. Edit to each of the following files and adjust the path to your Git checkout:
+
+    * `~/Library/PDF Services/Zebra Printer.workflow/Contents/document.wflow`
+    * `~/Library/Services/Zebra Printer.workflow/Contents/document.wflow`
+    * `~/Applications/Zebra Printer.app/Contents/document.wflow`
+
+   In each file, search for `/Users/schlomoschapiro/src/magic-zebra-printer` and replace it with the path where you checked out the Git repo.
+
+Now you should be able to use the Zebra Printer as a PDF Print Action, as a Service in other apps and Finder and as a standalone application which can serve as a drag-and-drop target.
+
+For Linux and Windows I'm happy to accept your contribution with a suitable integration.
+
+## Usage
+
+Simple open a PDF file with the Zebra Printer application or use the Zebra Printer service or PDF Service with it.
+
+## Bugs
+
+Lots. Not much error handling, no tests and I have no clue about MacOS internals and "just got it to work". So I'll be happy to get any feedback.
---
+++ magic-zebra-printer.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+
+"""
+   Copyright 2021 Schlomo Schapiro
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import sys, os, PyPDF2, math
+from sh import lp, lpstat
+from notifypy import Notify
+
+
+def notify(msg, title="Printing"):
+    notification = Notify(
+        default_notification_title=title,
+        default_application_name="Magic Zebra Printer",
+    )
+    notification.message = msg
+    notification.send(block=False)
+
+def getPrinter():
+    if "MAGIC_ZEBRA_PRINTER" in os.environ:
+        return os.environ.get("MAGIC_ZEBRA_PRINTER")
+    for line in filter(lambda l: "idle" in l, lpstat("-p")):
+        printer = line.split(" ")[1]
+        if "zebra" in printer.lower():
+            return printer
+
+def getSize(page):
+    box = page.cropBox
+    return (box.getWidth(), box.getHeight())
+
+def getRotation(page: PyPDF2.pdf.PageObject):
+    rotation = page.get("/Rotate")
+    if rotation is None:
+        rotation = 0
+    return rotation
+
+def die(msg):
+    print(msg,file=sys.stderr)
+    notify(msg)
+    sys.exit(1)
+
+
+def processPdfFile(pdfFile):
+    inPdf = PyPDF2.PdfFileReader(open(pdfFile, "rb"))
+    page = inPdf.getPage(0)
+    rotation = getRotation(page)
+
+    if rotation == 0:
+        width, height = getSize(page)
+        (shift_x, shift_y) = page.cropBox.lowerLeft
+        shift_x *= -1
+        shift_y *= -1
+
+    elif rotation == 270:
+        height, width = getSize(page)
+        (shift_y, shift_x) = page.cropBox.upperLeft
+        shift_y *= -1
+
+    elif rotation == 180:
+        width, height = getSize(page)
+        (shift_x, shift_y) = page.cropBox.upperRight
+
+    elif rotation == 90:
+        height, width = getSize(page)
+        (shift_y, shift_x) = page.cropBox.lowerRight
+        shift_x *= -1
+
+    else:
+        die(f"BAD ROTATION {rotation}")
+
+    print_width = 4 * 72  # 4 inch * 72 points-per-inch
+    scale_factor = print_width / width
+    print_height = math.ceil(height * scale_factor)
+
+    shift_x *= scale_factor
+    shift_y *= scale_factor
+
+    info = f"{width:.1f}×{height:.1f} {rotation}° ⇒ {print_width}x{print_height}\n▶ {shift_x:.1f},{shift_y:.1f} {scale_factor:.1%}"
+    print(pdfFile)
+    print(info)
+
+    outPdf = PyPDF2.PdfFileWriter()
+    for inPage in inPdf.pages:
+        outPage = outPdf.addBlankPage(print_width, print_height)
+        outPage.mergeRotatedScaledTranslatedPage(
+            inPage, 360 - rotation, scale_factor, shift_x, shift_y, False
+        )
+
+    outPdfFile = pdfFile + "_print.pdf"
+    with open(outPdfFile, "wb") as f:
+        outPdf.write(f)
+
+
+    return outPdfFile, print_width, print_height, info
+
+def printPdf(pdfFile, width, height, printer):
+    lp("-d", printer, "-o", f"PageSize=Custom.{width}x{height}", pdfFile)
+
+
+if __name__ == "__main__":
+    printer = getPrinter()
+    print(f"Using printer {printer}")
+    try:
+        pdfFile = sys.argv[1]
+    except:
+        die("1st arg is PDF file")
+
+    filename = os.path.basename(pdfFile)
+
+    outPdfFile, print_width, print_height, info = processPdfFile(pdfFile)
+
+    if len(sys.argv) > 2 and sys.argv[2] == "-noprint":
+        notify(info, title=f"Not Printing on {printer}: " + filename)
+    else:
+        printPdf(outPdfFile, print_width, print_height, printer)
+        os.remove(outPdfFile)
+        notify(info, title=f"Printing on {printer}: " + filename)
+
+
+
